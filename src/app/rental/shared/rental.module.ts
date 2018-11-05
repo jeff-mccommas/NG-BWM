e70d4f2fc091e30f3b46396abea21aeacfc665df
jeff-mccommas <jeffmccommas@gmail.com>
2018-11-05 16:31:06 -0500

    add http client

--- src/app/rental/shared/rental.module.ts
+++ src/app/rental/shared/rental.module.ts
@@ -1,19 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {BrowserModule} from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
-import {RentalListComponent} from '../rental-list/rental-list.component';
-import {RentalListItemComponent} from '../rental-list-item/rental-list-item.component';
-import {RentalComponent} from '../rental.component';
-import { RentalService } from './rental.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RentailDetailComponent } from '../rentail-detail/rentail-detail.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule, Routes } from "@angular/router";
+import { RentalListComponent } from "../rental-list/rental-list.component";
+import { RentalListItemComponent } from "../rental-list-item/rental-list-item.component";
+import { RentalComponent } from "../rental.component";
+import { RentalService } from "./rental.service";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RentailDetailComponent } from "../rentail-detail/rentail-detail.component";
+import { HttpClientModule } from "@angular/common/http";
 export const routes: Routes = [
-  { path: 'rentals', component: RentalComponent },
-  { path: '', component: RentalListComponent,
+  { path: "rentals", component: RentalComponent },
+  {
+    path: "",
+    component: RentalListComponent,
     children: [
-      { path: '', component: RentalListComponent },
-      { path: 'rentalId', component: RentailDetailComponent }
+      { path: "", component: RentalListComponent },
+      { path: "rentalId", component: RentailDetailComponent }
     ]
   }
 ];
@@ -21,25 +24,17 @@ export const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
+    HttpClientModule,
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule.forChild(routes)
   ],
   providers: [RentalService],
-  declarations: [  RentalListComponent,
+  declarations: [
+    RentalListComponent,
     RentalListItemComponent,
     RentailDetailComponent,
     RentalComponent
   ]
-
 })
-export class RentalModule {
-}
-
-
-
-
-
-
-
-
+export class RentalModule {}
